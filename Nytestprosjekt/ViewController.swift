dee2c057f0e9f9199ce56c2ac98686b48cd4d3af
Manveer Singh Pandher <manvep@netcompany.com>
2018-12-14 10:12:48 +0100

    Initial Commit

---
+++ Nytestprosjekt/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Nytestprosjekt
+//
+//  Created by Manveer Singh Pandher on 14/12/2018.
+//  Copyright © 2018 Manveer Singh Pandher. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
